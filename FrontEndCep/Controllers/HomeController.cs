a77f7fc9dc8a49df35017e118cd5e5c1e6b21ef6
Adao Ribeiro <sarinjr1024@hotmail.com>
2021-03-01 20:55:56 -0400

    FrontEndCepApi

---
+++ FrontEndCep/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using FrontEndCep.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace FrontEndCep.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult BuscarCEP(string cep)
+        {
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://localhost:8080/api/buscarcep/" + cep);
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var retorno = JsonConvert.DeserializeObject<Endereco>(streamReader.ReadToEnd());
+                return View(retorno);
+
+            }
+        }
+
+    }
+}
---
+++ FrontEndCep/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As Informações Gerais sobre um assembly são controladas por meio do 
+// conjunto de atributos a seguir. Altere esses valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("FrontEndCep")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FrontEndCep")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Configurar o ComVisible como false torna os tipos desse assembly invisíveis 
+// para componentes COM.  Se for necessário acessar um tipo nesse assembly a partir do 
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// A GUID a seguir será referente à ID do typelib se este projeto for exposto ao COM
+[assembly: Guid("fa991cb6-c22a-42a8-baca-36868ab4b1c1")]
+
+// As informações de versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou utilizar como padrão os Números de Revisão e da Versão 
+// usando o '*' como mostrado abaixo:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
